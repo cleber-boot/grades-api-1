5d4b3d5fc7900b12d32c31f41b6b3515ecda9926
cleber-boot <cleber.assisferreira@gmail.com>
2020-09-05 11:32:41 +0000

    gradeSchema

--- models/gradeModel.js
+++ models/gradeModel.js
@@ -23,7 +23,7 @@ export default (mongoose) => {
     },
   });
 
-    schema.method('toJSON', function () {
+  gradeSchema.method('toJSON', function () {
     const { __v, _id, ...object } = this.toObject();
 
     object.id = _id;
