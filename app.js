df32580c350452e0fb63007291a6ba7e56649805
cleber-boot <cleber.assisferreira@gmail.com>
2020-09-05 10:53:51 +0000

    cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://grades-app-cleber.herokuapp.com/',
+    origin: 'https://grades-app-cleber.herokuapp.com',
   })
 );
 app.use(gradeRouter);
